24a43ec2e015a561e4697663b69c88b416f00f51
Abel Chalier <abel.chalier@gmail.com>
2019-07-02 17:52:43 +0200

    removed methods with request

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ var config = {
   longCode: false
 };
 
-function init(conf) {
-  if (conf !== undefined)
-    config = require('merge').recursive(true, config, conf);
-  return module.exports;
-}
-
 function isTVA(number) {
   var numero = cleanSiret(number);
   if (numero.substring(0, 2) !== config.codePays) return false;
@@ -55,95 +49,6 @@ function getTvaKeyFromSiren(number) {
   return key;
 }
 
-function getCompanyInfo(number, callback) {
-  if (isNaN(number)) return false;
-  var isSiret = false;
-  var numero = cleanSiret(number);
-  if (checkSiret.isSIRET(number)) isSiret = true;
-  else if (checkSiret.isSIREN(number)) isSiret = false;
-  else return false;
-  if (isSiret) {
-    numero = numero.substring(0, 9);
-  }
-  var defaultCallback = function (err, data) {
-    if (err) {
-      console.error(err);
-    }
-    else {
-      console.log(data);
-    }
-  };
-  var cb = callback || defaultCallback;
-  var request = require('request');
-  var url = config.firmApiBase + config.firmApiSearch;
-  request({url: url + numero, json: true}, function (error, response, body) {
-    if (error) {
-      cb(error.message);
-    }
-    else if (body && body.status === "error") {
-      cb("error api : " + body.message);
-    }
-    else if (!response || response.statusCode !== 200) {
-      cb("error http code : " + response.statusCode);
-    }
-    else if (!body || body.status !== "success") {
-      cb("unknown error api : " + body.status);
-    }
-    else {
-      cb(null, body.company);
-    }
-  });
-}
-function getCompanyInfoNom(number, callback) {
-  getCompanyInfo(number,function (err, data) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      callback(null,data.names.best);
-    }
-  });
-}
-function getCompanyInfoAdress(number, callback) {
-  getCompanyInfo(number,function (err, data) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      callback(null,{add: data.address,cp: data.postal_code,ville: data.city} );
-    }
-  });
-}
-function getCompanyInfoCapital(number, callback) {
-  getCompanyInfo(number,function (err, data) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      callback(null,data.capital);
-    }
-  });
-}
-function getCompanyInfoLegal(number, callback) {
-  getCompanyInfo(number,function (err, data) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      callback(null,data.legal_form);
-    }
-  });
-}
-function getCompanyInfoLastUpdate(number, callback) {
-  getCompanyInfo(number,function (err, data) {
-    if (err) {
-      callback(err);
-    }
-    else {
-      callback(null,data.last_legal_update);
-    }
-  });
-}
 
 function cleanSiret(number) {
   var numero = new String(number);
@@ -151,17 +56,9 @@ function cleanSiret(number) {
   return numero;
 }
 
-module.exports = init;
-module.exports.config = config;
 module.exports.siret2tva = convertSiren2Tva;
 module.exports.siren2tva = convertSiren2Tva;
 module.exports.tva2siren = convertTva2Siren;
-module.exports.getInfo = getCompanyInfo;
-module.exports.getInfoNom = getCompanyInfoNom;
-module.exports.getInfoAdress = getCompanyInfoAdress;
-module.exports.getInfoCapital = getCompanyInfoCapital;
-module.exports.getInfoLegal = getCompanyInfoLegal;
-module.exports.getInfoLastUpdate = getCompanyInfoLastUpdate;
 module.exports.cleanSiret = cleanSiret;
 module.exports.cleanSiren = cleanSiret;
 module.exports.cleanTva = cleanSiret;
